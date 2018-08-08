632d11440b77e7a84c13b88c75d55b9177b1f107
arminsu7 <734722864@qq.com>
2018-08-08 17:04:58 +0800

    debug

--- src/arith_uint256.cpp
+++ src/arith_uint256.cpp
@@ -262,7 +262,7 @@ arith_uint256 UintToArith256(const uint256 &a)
 arith_uint256 maxUint256Div(const uint256 &a)
 {
     arith_uint256 d = UintToArith256(a);
-    assert(d == 0);
+    assert(d != 0);
     arith_uint256 t= UintToArith256(maxUint256) / d;
     return t;
 }
