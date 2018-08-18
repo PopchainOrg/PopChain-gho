8a47cfcef2adebf2c2439f28ec93fabd9c0a07f4
arminsu7 <734722864@qq.com>
2018-08-18 16:44:48 +0800

    add getblockdifficulty rpc command

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -345,6 +345,32 @@ UniValue getblockhash(const UniValue& params, bool fHelp)
     return pblockindex->GetBlockHash().GetHex();
 }
 
+UniValue getblockdifficulty(const UniValue& params, bool fHelp)
+{
+    if (fHelp || params.size() != 1)
+        throw runtime_error(
+            "getblockdifficulty index\n"
+            "\nReturns difficulty of block in best-block-chain at index provided.\n"
+            "\nArguments:\n"
+            "1. index         (numeric, required) The block index\n"
+            "\nResult:\n"
+            "\"difficulty\"         (string) The block difficulty\n"
+            "\nExamples:\n"
+            + HelpExampleCli("getblockdifficulty", "1000")
+            + HelpExampleRpc("getblockdifficulty", "1000")
+        );
+
+    LOCK(cs_main);
+
+    int nHeight = params[0].get_int();
+    if (nHeight < 0 || nHeight > chainActive.Height())
+        throw JSONRPCError(RPC_INVALID_PARAMETER, "Block height out of range");
+
+    CBlockIndex* pblockindex = chainActive[nHeight];
+    //return pblockindex->GetBlockHash().GetHex();
+    return to_string(GetDifficulty(pblockindex));
+}
+
 UniValue getblockheader(const UniValue& params, bool fHelp)
 {
     if (fHelp || params.size() < 1 || params.size() > 2)
